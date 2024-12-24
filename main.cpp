41f554e238f08938b4bea9a72c893d107cb5cf85
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-24 19:50:14 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int degree (int N, int D) {
+    if (D < 1) {
+        return 1;
+    }
+    return N * degree(N, D - 1);
+}
+
+int main() {
+
+    int N;
+    cout << "Enter a digit: ";
+    cin >> N;
+    int D;
+    cout << "Enter a degree: ";
+    cin >> D;
+
+    cout << degree(N, D) << endl;
+
+
+    return 0;
+}
